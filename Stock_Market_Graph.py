5be1c1b7086ab023835830130aef05afbabaa8c5
Wilson Chan <wilsonchan54@gmail.com>
2020-11-18 23:35:28 -0500

    Establish the necessary functions / create function #1 that collects stock data from Yahoo

--- Stock_Market_Graph.py
+++ Stock_Market_Graph.py
@@ -13,3 +13,25 @@ START_DATE = '2007-10-01'      #around the time of the Great Depression
 END_DATE = str(datetime.now().strftime('%Y-%m-%d'))        
 #currently using '2020-05-15'     			alternative: str(datetime.now().strftime('%Y-%m-%d))
 
+
+#four functions 
+#1) gets data
+#2) cleans data
+#3) creates plot/ graph
+#4) basic stats about the stock
+
+
+
+def get_data(ticker):        #function 1
+	try:
+		stock_data = data.DataReader(ticker,
+						'yahoo',
+						START_DATE,
+						END_DATE)
+		adj_close = clean_data(stock_data, 'Adj Close')           #adjusted close
+		create_plot(adj_close, ticker)
+
+	except RemoteDataError:
+		print("No data available for {t}".format(t = ticker))
+
+#-------------------------------------------------#
